728ddfbe7a9710fe8d5ecdff32c01b0552d138d3
spaceC00kie <kirstie317@gmail.com>
2020-02-16 16:12:17 -0500

    cleaned comments and added to dialogue

--- dog_morning.py
+++ dog_morning.py
@@ -1,37 +1,28 @@
-#two layers of if
 
-#ask user first question. store answer
 x = input("You wake up. Do you want to go potty or eat? Choose A or B\n")
 
-#if x == A potty Go bother human to be let out
 if x == "A":
-	#Ask user if they want to go on walk. Y or N
 
-	usr_ans = ""
+	usr_ans = "A"
 	#loop until usr_ans is Y or N
 	while usr_ans != "Y" and usr_ans != "N":
 		usr_ans = input("Do you want to go for a walk? Y or N?\n")
-		#if Y go on a walk and find a smell
 		if usr_ans == "Y":
-			print("You go on a walk and find a smell.")
-		#elif N stay home and watch tv
+			print("Your human takes you on a walk, you find a lot of smells, and you make some of your own.")
 		elif usr_ans == "N":
-			print("You stay home and watch tv.")
+			print("You are let into the yard to go potty then you come back inside. You're a very low energy dog")
 		else:
 			print("Please pick one, Y or N.")
 
-
-#elif x == B eat Go bother human for food
 elif x == "B":
-	#Ask if they want to go for walk. Y or N?
-	usr_ans = input ("Do you want breakfast? Y or N?\n")
-	#if Y go on a walk and potty
-	if usr_ans == "Y":
-		print("You bring you food bowl to your human.")
-	#elif N stay home and watch tv and wet the floor
-	elif usr_ans == "N":
-		print("You stay home and watch tv hungry until you wet the floor.")
-	#else Please pick one
-#else #please make a choice
+	#loop until usr_ans is Y or N
+	while usr_ans != "Y" and usr_ans != "N":
+		usr_ans = input ("Do you want to ask for breakfast like a good dog? Y or N?\n")
+		if usr_ans == "Y":
+			print("You bring you food bowl to your human. That's adorable, so your human feeds you immediately.")
+		elif usr_ans == "N":
+			print("You stand by the food and bark at the containter. Your owner is not going to let you tell them what to do by barking. You'll have to wait a while to eat now.")
+		else:
+			print("You said you wanted to eat. Are you a good dog or not?")
 
 
